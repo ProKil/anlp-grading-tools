fdf31db70bac8bffe034d9caa635ea5f7b4cc76e
Shubham Phal <shubhamphal@shubhams-mbp.myfiosgateway.com>
2022-09-12 12:32:53 -0400

    changes for minbert assignment

--- anlp_grading/test.py
+++ anlp_grading/test.py
@@ -5,11 +5,15 @@ data_folder = Path('/mnt/data')
 student_folder = Path('/mnt/code')
 scores_folder = Path('/mnt/scores')
 
-execute_cli_timeout('cd /mnt/code && python3 /mnt/code/setup.py', timeout=1800)
-execute_cli_timeout('cp /mnt/data/classifier_orig.py /mnt/code/classifier_orig.py', timeout=600)
+execute_cli_timeout('cd /mnt/code && bash /mnt/code/setup.sh', timeout=1800)
+#execute_cli_timeout('cp /mnt/data/classifier_orig.py /mnt/code/classifier_orig.py', timeout=600)
+
 execute_cli_timeout(
     'cd /mnt/code && '
     'python3 /mnt/code/classifier.py '
+    '--option=pretrain'
+    '--epochs=10'
+    '--lr=1e-3'
     '--train=/mnt/data/sst-train.txt '
     '--dev=/mnt/data/sst-dev.txt '
     '--test=/mnt/data/sst-test.txt '
@@ -17,21 +21,34 @@ execute_cli_timeout(
     '--test_out=/usr/src/app/sst-test-output.txt ',
     timeout=1800
 )
-sst_acc = compare_outputs(std="/mnt/data/sst-test.txt", result="/usr/src/app/sst-test-output.txt")
-execute_cli_timeout(
-    'cd /mnt/code && '
-    'python3 /mnt/code/classifier_orig.py '
-    '--train=/mnt/data/sst-train.txt '
-    '--dev=/mnt/data/sst-dev.txt '
-    '--test=/mnt/data/sst-test.txt '
-    '--dev_out=/usr/src/app/sst-dev-output-orig.txt '
-    '--test_out=/usr/src/app/sst-test-output-orig.txt ',
-    timeout=1800
-)
-sst_acc_orig = compare_outputs(std="/mnt/data/sst-test.txt", result="/usr/src/app/sst-test-output-orig.txt")
+
+sst_pretrain_dev_acc = compare_outputs(std="/mnt/data/sst-dev.txt", result="/usr/src/app/sst-dev-output.txt")
+sst_pretrain_test_acc = compare_outputs(std="/mnt/data/sst-test.txt", result="/usr/src/app/sst-test-output.txt")
+
 execute_cli_timeout(
     'cd /mnt/code && '
     'python3 /mnt/code/classifier.py '
+    '--option=finetune'
+    '--epochs=10'
+    '--lr=1e-5'
+    '--train=/mnt/data/sst-train.txt '
+    '--dev=/mnt/data/sst-dev.txt '
+    '--test=/mnt/data/sst-test.txt '
+    '--dev_out=/usr/src/app/sst-dev-output.txt '
+    '--test_out=/usr/src/app/sst-test-output.txt ',
+    timeout=1800
+)
+
+sst_finetune_dev_acc = compare_outputs(std="/mnt/data/sst-dev.txt", result="/usr/src/app/sst-dev-output.txt")
+sst_finetune_test_acc = compare_outputs(std="/mnt/data/sst-test.txt", result="/usr/src/app/sst-test-output.txt")
+
+#sst_acc_orig = compare_outputs(std="/mnt/data/sst-test.txt", result="/usr/src/app/sst-test-output-orig.txt")
+execute_cli_timeout(
+    'cd /mnt/code && '
+    'python3 /mnt/code/classifier.py '
+    '--option=finetune'
+    '--epochs=10'
+    '--lr=1e-5'
     '--train=/mnt/data/cfimdb-train.txt '
     '--dev=/mnt/data/cfimdb-dev.txt '
     '--test=/mnt/data/cfimdb-test.txt '
@@ -39,23 +56,29 @@ execute_cli_timeout(
     '--test_out=/usr/src/app/cfimdb-test-output.txt ',
     timeout=1800
 )
-cfimdb_acc = compare_outputs(std="/mnt/data/cfimdb-test-correctlabels.txt", result="/usr/src/app/cfimdb-test-output.txt")
-execute_cli_timeout(
-    'cd /mnt/code && '
-    'python3 /mnt/code/classifier_orig.py '
-    '--train=/mnt/data/cfimdb-train.txt '
-    '--dev=/mnt/data/cfimdb-dev.txt '
-    '--test=/mnt/data/cfimdb-test.txt '
-    '--dev_out=/usr/src/app/cfimdb-dev-output.txt '
-    '--test_out=/usr/src/app/cfimdb-test-output-orig.txt ',
-    timeout=1800
-)
-cfimdb_acc_orig = compare_outputs(std="/mnt/data/cfimdb-test-correctlabels.txt", result="/usr/src/app/cfimdb-test-output-orig.txt")
+cfimdb_finetune_dev_acc = compare_outputs(std="/mnt/data/cfimdb-dev-output.txt", result="/usr/src/app/cfimdb-dev-output.txt")
+# cfimdb_pretain_test_acc = compare_outputs(std="/mnt/data/sst-test.txt", result="/usr/src/app/sst-test-output.txt")
 
-print(sst_acc, sst_acc_orig, cfimdb_acc, cfimdb_acc_orig)
+# execute_cli_timeout(
+#     'cd /mnt/code && '
+#     'python3 /mnt/code/classifier.py '
+#     '--option=finetune'
+#     '--epochs=10'
+#     '--lr=1e-5'
+#     '--train=/mnt/data/cfimdb-train.txt '
+#     '--dev=/mnt/data/cfimdb-dev.txt '
+#     '--test=/mnt/data/cfimdb-test.txt '
+#     '--dev_out=/usr/src/app/cfimdb-dev-output.txt '
+#     '--test_out=/usr/src/app/cfimdb-test-output-orig.txt ',
+#     timeout=1800
+# )
+# cfimdb_pretrain_dev_acc = compare_outputs(std="/mnt/data/sst-dev.txt", result="/usr/src/app/sst-dev-output.txt")
+#cfimdb_pretain_test_acc = compare_outputs(std="/mnt/data/sst-test.txt", result="/usr/src/app/sst-test-output.txt")
+
+print(sst_pretrain_dev_acc, sst_pretrain_test_acc, sst_finetune_dev_acc, sst_finetune_test_acc,cfimdb_finetune_dev_acc)
 
 with open(scores_folder / os.environ["ANDREW_ID"], "w") as f:
-    f.write(f"{sst_acc}\t{sst_acc_orig}\t{cfimdb_acc}\t{cfimdb_acc_orig}")
+    f.write(f"{sst_pretrain_dev_acc}\t{sst_pretrain_test_acc}\t{sst_finetune_dev_acc}\t{sst_finetune_test_acc}\t{cfimdb_finetune_dev_acc}")
 
 execute_cli_timeout(
     'rm -rf /mnt/code/*',
