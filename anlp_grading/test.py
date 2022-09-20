07c442dddd0d80c2c834700eb86ba18bb1c761e3
Shubham Phal <shubhamphal@shubhams-mbp-2.myfiosgateway.com>
2022-09-20 01:54:03 -0400

    Modified test.py

--- anlp_grading/test.py
+++ anlp_grading/test.py
@@ -5,8 +5,12 @@ data_folder = Path('/mnt/data')
 student_folder = Path('/mnt/code')
 scores_folder = Path('/mnt/scores')
 
+print('Running setup.sh')
+
 execute_cli_timeout('cd /mnt/code && bash /mnt/code/setup.sh', timeout=1800)
 #execute_cli_timeout('cp /mnt/data/classifier_orig.py /mnt/code/classifier_orig.py', timeout=600)
+
+print('Running pretrain')
 
 execute_cli_timeout(
     'cd /mnt/code && '
@@ -24,6 +28,9 @@ execute_cli_timeout(
 
 sst_pretrain_dev_acc = compare_outputs(std="/mnt/data/sst-dev.txt", result="/usr/src/app/sst-dev-output.txt")
 sst_pretrain_test_acc = compare_outputs(std="/mnt/data/sst-test.txt", result="/usr/src/app/sst-test-output.txt")
+
+
+print('Running finetune')
 
 execute_cli_timeout(
     'cd /mnt/code && '
@@ -56,6 +63,10 @@ execute_cli_timeout(
     '--test_out=/usr/src/app/cfimdb-test-output.txt ',
     timeout=1800
 )
+
+
+print('Running cfimdb finetune')
+
 cfimdb_finetune_dev_acc = compare_outputs(std="/mnt/data/cfimdb-dev-output.txt", result="/usr/src/app/cfimdb-dev-output.txt")
 # cfimdb_pretain_test_acc = compare_outputs(std="/mnt/data/sst-test.txt", result="/usr/src/app/sst-test-output.txt")
 
