b600a836e808b00efb959721da7faed708323c30
Shubham Phal <shubhamphal@shubhams-mbp-2.myfiosgateway.com>
2022-09-21 10:18:08 -0400

    Added anlp

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 docker build . -t anlp
 export ANLP_TMP_DIR=/home/ubuntu/anlp-grading-tools/tmp
-export SUBMISSION_DIR=//home/ubuntu/anlp-grading-tools/submission/
+export SUBMISSION_DIR=/home/ubuntu/anlp-grading-tools/submission/
 export DATA_DIR=/home/ubuntu/anlp-grading-tools/data
 export SCORES_DIR=/home/ubuntu/anlp-grading-tools/scores
 python3 anlp_grading/main.py
