2736d9176be1d4ccd82c8ba324a7782e99bc68ea
Shubham Phal <shubhamphal@shubhams-mbp-2.myfiosgateway.com>
2022-09-21 10:30:48 -0400

    Added anlp

--- anlp_grading/setup.sh
+++ anlp_grading/setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+conda init bash
 conda create -n bert_hw -y python=3.7
 conda activate bert_hw
-conda install pytorch==1.8.0 torchvision torchaudio cudatoolkit=10.1 -c pytorch
+conda install -y pytorch==1.8.0 torchvision torchaudio cudatoolkit=10.1 -c pytorch
 pip install tqdm==4.58.0
 pip install requests==2.25.1
 pip install importlib-metadata==3.7.0
